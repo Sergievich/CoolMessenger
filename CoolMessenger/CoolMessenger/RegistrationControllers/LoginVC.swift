2bdb62e238a8a46d94c86beda99ed420e7c398c2
Sergievich <segievich.air@gmail.com>
2021-10-26 17:05:37 +0300

    SignUpVC start created

--- CoolMessenger/CoolMessenger/RegistrationControllers/LoginVC.swift
+++ CoolMessenger/CoolMessenger/RegistrationControllers/LoginVC.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginVC: UIViewController{
+    
+    let welcomLbl = UILabel(text: "Welcom Back", font: .avenir26())
+    
+    let loginLbl = UILabel(text: "Login with", font: .avenir20())
+    let googlenBtn = UIButton(backgoundColor: .white, titleColot: .black, title: "Google", isShadow: true, font: .avenir20(), cornerRadius: 4)
+    
+    let orLbl = UILabel(text: "or", font: .avenir20())
+    
+    let emailLbl = UILabel(text: "Email", font: .avenir20())
+    let emailTF = OneLineTF(font: .avenir20())
+    
+    let passwordLbl = UILabel(text: "Password", font: .avenir20())
+    let passwordTF = OneLineTF(font: .avenir20())
+    
+    let loginBtn = UIButton(backgoundColor: .blue, titleColot: .white, title: "Login", isShadow: true, font: .avenir20(), cornerRadius: 4)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+      
+}
+
+
+//MARK: SwiftUI
+import SwiftUI
+
+struct LoginVCProvider: PreviewProvider{
+  static var previews: some View{
+      ContainerView().edgesIgnoringSafeArea(.all)
+  }
+  
+  struct ContainerView: UIViewControllerRepresentable{
+    
+      
+      let loginVC  = LoginVC()
+      
+      func makeUIViewController(context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) -> LoginVC {
+          return loginVC
+      }
+      
+      func updateUIViewController(_ uiViewController: LoginVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) {
+          
+      }
+  }
+}
--- CoolMessenger/CoolMessenger/SceneDelegate.swift
+++ CoolMessenger/CoolMessenger/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = HelloVC()
+        window?.rootViewController = LoginVC()
         window?.makeKeyAndVisible()
     }
 
